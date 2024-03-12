4f6be3441896d3717cd70953b82cdcfcbabc8540
Gabriela Batista Pauka <gabrielabpauka@hotmail.com>
2024-03-12 20:23:58 -0300

    Criando a classe MainList

---
+++ projeto-list/src/br/com/projetolist/MainList.java
@@ -0,0 +1,46 @@
+package br.com.projetolist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainList {
+
+	public static void main(String[] args) {
+		
+		// Declarar e inicializar uma Lista
+		List<String> listaNomes = new ArrayList<String>();
+		
+		// Descobrir o tamanho da lista
+		System.out.println(listaNomes.size());
+		
+		// Aplicando CRUD
+		
+		// Create: Adicionar elementos a lista
+		listaNomes.add("Harry");
+		listaNomes.add("Rony");
+		listaNomes.add("Hermione");
+		listaNomes.add("Luna");
+		
+		// Read: Listar Elementos
+		System.out.println("- Lista de presença:");
+		for (String nome : listaNomes) {
+			System.out.println(nome);
+		}
+		
+		// Encontrar o index de um elemento
+		int indexAlterar = listaNomes.indexOf("Rony");
+		int indexExcluir = listaNomes.indexOf("Luna");
+		
+		// Edit: Editar Elementos
+		listaNomes.set(indexAlterar, "Albus");
+		
+		// Delete: Remover Elementos
+		listaNomes.remove(indexExcluir);
+		
+		System.out.println("-----------");
+		for (String nome : listaNomes) {
+			System.out.println(nome);
+		}
+	}
+
+}
